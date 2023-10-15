8c1b6c3ba7b45ac50c6f165b8d33d9a4e16061c9
unknown <mohseen_abasi@yahoo.com>
2023-10-15 11:12:58 +0330

    remove console.log

--- app/libs/users/addUser.tsx
+++ app/libs/users/addUser.tsx
@@ -1,8 +1,8 @@
 type newUserType = { name: string; email: string; phone: string };
 
 export const addUser = async (newUser: newUserType) => {
-  console.log(newUser, " NewUserdata in add user function query file");
-  const { name, email, phone } = newUser;
+  // console.log(newUser, " NewUserdata in add user function query file");
+  // const { name, email, phone } = newUser;
 
   const data = await fetch(`${process.env.NEXT_PUBLIC_ENV_API_URL}/users`, {
     method: "POST",
@@ -11,6 +11,6 @@ export const addUser = async (newUser: newUserType) => {
   });
 
   const res = await data.json();
-  console.log(res);
+  // console.log(res);
   return res;
 };
