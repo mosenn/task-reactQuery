c017fa5ac781d74d6d1286c53e4c2e6cd87f8e73
unknown <mohseen_abasi@yahoo.com>
2023-12-19 21:33:38 +0330

    try to ssr homepage

--- app/components/homePageDisplay/HomePageDisplay.tsx
+++ app/components/homePageDisplay/HomePageDisplay.tsx
@@ -3,59 +3,31 @@ import React from "react";
 import Title from "../Title";
 import LinkContainer from "../LinkContainer";
 import usePagenation from "../Pagenation";
-import DisplayUser from "../DisplayUser";
+// import DisplayUser from "../DisplayUser";
 import { AiOutlineArrowLeft, AiOutlineArrowRight } from "react-icons/ai";
+import { HomeUi } from "../home ui/HomeUi";
 
-type usersType = {
-  name: string;
-  phone: string;
-  email: string;
-  id: string;
-};
+// type usersType = {
+//   name: string;
+//   phone: string;
+//   email: string;
+//   id: string;
+// };
 
 const HomePageDisplay = () => {
   const { isLoading, data, nextPage, prevPage, page, perPage } =
     usePagenation();
-
-  return (
-    <div>
-      <Title text="Welcome 🎉" />
-      <div className="md:mx-3">
-        <LinkContainer address="/add-user" text="add user " sizeText="2xl" />
-      </div>
-      {isLoading ? (
-        <h1>Loading...</h1>
-      ) : (
-        <div className=" md:grid md:grid-cols-2 lg:grid-cols-3">
-          {data?.map((user: usersType) => {
-            const { id } = user;
-            return (
-              <div key={id}>
-                <DisplayUser {...user} />
-              </div>
-            );
-          })}
-        </div>
-      )}
-      <section className="flex justify-center items-center p-3">
-        <button onClick={prevPage} disabled={page === 1}>
-          <AiOutlineArrowLeft
-            size={20}
-            fill={+page === 1 && "rgb(200, 200, 200)"}
-            className="hover:fill-cyan-600"
-          />
-        </button>{" "}
-        <div className="mx-3 text-xl">{page}</div>
-        <button onClick={nextPage}>
-          <AiOutlineArrowRight
-            className="hover:fill-cyan-600"
-            size={20}
-            fill={data?.length >= perPage ? "black" : "rgb(200, 200, 200)"}
-          />
-        </button>
-      </section>
-    </div>
-  );
+  const uiHomeProps = {
+    isLoading,
+    data,
+    nextPage,
+    prevPage,
+    page,
+    perPage,
+    AiOutlineArrowLeft,
+    AiOutlineArrowRight,
+  };
+  return <HomeUi dataui={uiHomeProps} />;
 };
 
 export default HomePageDisplay;
